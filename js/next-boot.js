9b37cb778c2e3dd04b1afddc95d320dae06235c4
Pei Jia <jiapei@longervision.com>
2025-07-11 14:09:52 -0700

    Refreshed

--- js/next-boot.js
+++ js/next-boot.js
@@ -6,7 +6,6 @@ NexT.boot.registerEvents = function() {
 
   NexT.utils.registerScrollPercent();
   NexT.utils.registerCanIUseTag();
-  NexT.utils.updateFooterPosition();
 
   // Mobile top menu bar.
   document.querySelector('.site-nav-toggle .toggle').addEventListener('click', event => {
@@ -27,7 +26,7 @@ NexT.boot.registerEvents = function() {
     const tHash = location.hash;
     if (tHash !== '' && !tHash.match(/%\S{2}/)) {
       const target = document.querySelector(`.tabs ul.nav-tabs li a[href="${tHash}"]`);
-      target?.click();
+      target && target.click();
     }
   });
 
@@ -65,8 +64,8 @@ NexT.boot.motion = function() {
   if (CONFIG.motion.enable) {
     NexT.motion.integrator
       .add(NexT.motion.middleWares.header)
-      .add(NexT.motion.middleWares.sidebar)
       .add(NexT.motion.middleWares.postList)
+      .add(NexT.motion.middleWares.sidebar)
       .add(NexT.motion.middleWares.footer)
       .bootstrap();
   }
